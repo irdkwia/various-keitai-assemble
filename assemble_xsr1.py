58f9e7992531458e7f2965bd1bc5a340864df7f4
irdkwia <irdkwia2000@outlook.com>
2025-11-18 19:27:20 +0100

    Refactor XSR1 and XSR2

--- assemble_xsr1.py
+++ assemble_xsr1.py
@@ -21,35 +21,61 @@ out_oob = args.input_oob or os.path.join(
 
 os.makedirs(args.output, exist_ok=True)
 
-block_oldness = {}
-block_select = {}
 with open(args.input, "rb") as nand:
     with open(out_oob, "rb") as oob:
-        data = nand.read(0x8000)
-        while len(data) > 0:
-            spare = oob.read(0x400)
-            if data[0xC:0x10] == b"XSR1" and spare[4] != 0:
-                sector = int.from_bytes(data[0x10:0x14], "little")
-                version = int.from_bytes(data[0x14:0x18], "little")
-                for area in range(0x3F):
-                    spare_offset = 0x10 + area * 0x10
-                    if spare[spare_offset + 0x3] != 0xFF:
-                        block_id = int.from_bytes(
-                            spare[spare_offset : spare_offset + 0x3], "little"
-                        )
-                        if (
-                            block_id not in block_oldness
-                            or block_oldness[block_id] <= version
-                        ):
-                            block_select[block_id] = data[
-                                0x200 * (area + 1) : 0x200 * (area + 2)
-                            ]
-                            block_oldness[block_id] = version
-            data = nand.read(0x8000)
-with open(os.path.join(args.output, "region.bin"), "wb") as file:
-    last_block = 0
-    for block_id, block_data in sorted(block_select.items()):
-        if last_block != block_id:
-            file.write(bytes([0xFF] * (block_id - last_block) * 0x200))
-        file.write(block_data)
-        last_block = block_id + 1
+        data = b"\x00" * 0x10
+        while data[:0xC] != b"XSRPARTI\x00\x10\x01\x00" and len(data) > 0:
+            data = nand.read(0x10)
+        if len(data) > 0:
+            part_table = nand.read(0x10 * int.from_bytes(data[0xC:], "little"))
+            for part in range(0, len(part_table), 0x10):
+                part_nb = int.from_bytes(part_table[part : part + 4], "little")
+                sector_size = 0x4000
+                sector_start = int.from_bytes(
+                    part_table[part + 8 : part + 12], "little"
+                )
+                sector_nb = int.from_bytes(part_table[part + 12 : part + 16], "little")
+                nand.seek(sector_size * sector_start)
+                oob.seek(sector_size * sector_start // 0x20)
+                with open(
+                    os.path.join(args.output, "partition_%08X.bin" % part_nb), "wb"
+                ) as file:
+                    if not part_table[part + 4] & 1:
+                        file.write(nand.read(sector_size * sector_nb))
+                    else:
+                        block_oldness = {}
+                        block_select = {}
+                        sector_id = 0
+                        while sector_id < sector_nb:
+                            data = nand.read(sector_size)
+                            spare = oob.read(sector_size // 0x20)
+                            sector_id += 1
+                            if data[0xC:0x10] == b"XSR1" and spare[4] != 0:
+                                data += nand.read(sector_size)
+                                spare += oob.read(sector_size // 0x20)
+                                sector_id += 1
+                                sector = int.from_bytes(data[0x10:0x14], "little")
+                                version = int.from_bytes(data[0x14:0x18], "little")
+                                for area in range(0x3F):
+                                    spare_offset = 0x10 + area * 0x10
+                                    if spare[spare_offset + 0x3] != 0xFF:
+                                        block_id = int.from_bytes(
+                                            spare[spare_offset : spare_offset + 0x3],
+                                            "little",
+                                        )
+                                        if (
+                                            block_id not in block_oldness
+                                            or block_oldness[block_id] <= version
+                                        ):
+                                            block_select[block_id] = data[
+                                                0x200 * (area + 1) : 0x200 * (area + 2)
+                                            ]
+                                            block_oldness[block_id] = version
+                        last_block = 0
+                        for block_id, block_data in sorted(block_select.items()):
+                            if last_block != block_id:
+                                file.write(
+                                    bytes([0xFF] * (block_id - last_block) * 0x200)
+                                )
+                            file.write(block_data)
+                            last_block = block_id + 1
--- assemble_xsr2.py
+++ assemble_xsr2.py
@@ -21,60 +21,80 @@ out_oob = args.input_oob or os.path.join(
 
 os.makedirs(args.output, exist_ok=True)
 
-block_list = {}
-sector_oldness = {}
-block_select = {}
 with open(args.input, "rb") as nand:
     with open(out_oob, "rb") as oob:
-        addr = 0
-        data = nand.read(0x20000)
-        while len(data) > 0:
-            spare = oob.read(0x1000)
-            if data[0xC:0x10] == b"XSR2" and spare[6] != 0:
-                partition = int.from_bytes(data[0x40:0x44], "little")
-                sector_oldness[partition] = sector_oldness.get(partition, {})
-                block_list[partition] = block_list.get(partition, {})
-                block_select[partition] = block_select.get(partition, {})
-                sector = int.from_bytes(data[0x10:0x14], "little")
-                block = int.from_bytes(data[0x14:0x16], "little")
-                version = int.from_bytes(data[0x16:0x18], "little")
-                overwrite = False
-                if sector not in sector_oldness[partition] or sector_oldness[partition][
-                    sector
-                ] < (block | (version << 16)):
-                    overwrite = True
-                    sector_oldness[partition][sector] = block | (version << 16)
-                    for area in range(0x7E):
-                        for split in range(0x2):
-                            block_id = sector * 0xFC + area * 2 + split
-                            if data[0x100 + area * 0x2 + 1] >= 0xFE:
-                                block_select[partition][block_id] = None
-                            else:
-                                block_select[partition][block_id] = (
-                                    data[0x100 + area * 0x2] & 0x7F
+        data = b"\x00" * 0x10
+        while data[:0xC] != b"XSRPARTI\x00\x10\x01\x00" and len(data) > 0:
+            data = nand.read(0x10)
+        if len(data) > 0:
+            part_table = nand.read(0x10 * int.from_bytes(data[0xC:], "little"))
+            for part in range(0, len(part_table), 0x10):
+                part_nb = int.from_bytes(part_table[part : part + 4], "little")
+                sector_size = 0x20000
+                sector_start = int.from_bytes(
+                    part_table[part + 8 : part + 12], "little"
+                )
+                sector_nb = int.from_bytes(part_table[part + 12 : part + 16], "little")
+                nand.seek(sector_size * sector_start)
+                oob.seek(sector_size * sector_start // 0x20)
+                with open(
+                    os.path.join(args.output, "partition_%08X.bin" % part_nb), "wb"
+                ) as file:
+                    if not part_table[part + 4] & 1:
+                        file.write(nand.read(sector_size * sector_nb))
+                    else:
+                        block_list = {}
+                        sector_oldness = {}
+                        block_select = {}
+                        sector_id = 0
+                        while sector_id < sector_nb:
+                            data = nand.read(sector_size)
+                            spare = oob.read(sector_size // 0x20)
+                            sector_id += 1
+                            if (
+                                data[0xC:0x10] == b"XSR2"
+                                and spare[6] != 0
+                                and int.from_bytes(data[0x40:0x44], "little") == part_nb
+                            ):
+                                sector = int.from_bytes(data[0x10:0x14], "little")
+                                block = int.from_bytes(data[0x14:0x16], "little")
+                                version = int.from_bytes(data[0x16:0x18], "little")
+                                overwrite = False
+                                if sector not in sector_oldness or sector_oldness[
+                                    sector
+                                ] < (block | (version << 16)):
+                                    overwrite = True
+                                    sector_oldness[sector] = block | (version << 16)
+                                    for area in range(0x7E):
+                                        for split in range(0x2):
+                                            block_id = sector * 0xFC + area * 2 + split
+                                            if data[0x100 + area * 0x2 + 1] >= 0xFE:
+                                                block_select[block_id] = None
+                                            else:
+                                                block_select[block_id] = (
+                                                    data[0x100 + area * 0x2] & 0x7F
+                                                )
+                                for area in range(0xFC):
+                                    sub_id = spare[0x40 + area * 0x10 + 0x2]
+                                    if sub_id ^ 0xFF == spare[0x40 + area * 0x10 + 0x3]:
+                                        block_id = sector * 0xFC + sub_id
+                                        block_list[block_id] = block_list.get(
+                                            block_id, {}
+                                        )
+                                        block_list[block_id][block] = data[
+                                            0x800 + 0x200 * area : 0xA00 + 0x200 * area
+                                        ]
+                                        if overwrite:
+                                            block_select[block_id] = block
+                        last_block = 0
+                        for block_id, block_data in sorted(block_list.items()):
+                            if last_block != block_id:
+                                file.write(
+                                    bytes([0xFF] * (block_id - last_block) * 0x200)
                                 )
-                for area in range(0xFC):
-                    sub_id = spare[0x40 + area * 0x10 + 0x2]
-                    if sub_id ^ 0xFF == spare[0x40 + area * 0x10 + 0x3]:
-                        block_id = sector * 0xFC + sub_id
-                        block_list[partition][block_id] = block_list[partition].get(
-                            block_id, {}
-                        )
-                        block_list[partition][block_id][block] = data[
-                            0x800 + 0x200 * area : 0xA00 + 0x200 * area
-                        ]
-                        if overwrite:
-                            block_select[partition][block_id] = block
-            data = nand.read(0x20000)
-            addr += 0x20000
-
-for partition, blocks in sorted(block_list.items()):
-    with open(os.path.join(args.output, "region_%04d.bin" % partition), "wb") as file:
-        last_block = 0
-        for block_id, block_data in sorted(blocks.items()):
-            if last_block != block_id:
-                file.write(bytes([0xFF] * (block_id - last_block) * 0x200))
-            file.write(
-                block_data.get(block_select[partition][block_id], bytes([0xFF] * 0x200))
-            )
-            last_block = block_id + 1
+                            file.write(
+                                block_data.get(
+                                    block_select[block_id], bytes([0xFF] * 0x200)
+                                )
+                            )
+                            last_block = block_id + 1
