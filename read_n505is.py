e1e2356eae24160f21a88a2f0f9782f23c96196a
irdkwia <irdkwia2000@outlook.com>
2026-02-06 11:42:37 +0100

    Read N505iS support N505i config

--- read_n505is.py
+++ read_n505is.py
@@ -4,25 +4,44 @@ import os
 parser = argparse.ArgumentParser(description="Keitai N505iS FileSystem Read")
 parser.add_argument("input")
 parser.add_argument("output")
+parser.add_argument(
+    "-c",
+    "--config",
+    help="Choose configuration. Accepted values: N505i, N505iS",
+    default="N505iS",
+    type=str,
+)
 
 args = parser.parse_args()
 
 with open(args.input, "rb") as file:
     data = file.read()
 
+if args.config == "N505i":
+    LIST_START = 0xDAD200
+    LIST_END = 0xDC6800
+    TABLE = 0xDC6800
+    RANGES = [
+        (0, ("unknown",), None, 0),
+    ]
+elif args.config == "N505iS":
+    LIST_START = 0x800
+    LIST_END = 0x1E000
+    TABLE = 0x1E40C
+    RANGES = [
+        (0, ("unknown",), None, 0),
+        (12484, ("unknown",), "bin", 12484),
+        (12492, ("JAVA", "JAR"), "jar", 0),
+        (12687, ("unknown",), "bin", 12687),
+        (12692, ("JAVA", "SCP"), "scp", 0),
+        (12887, ("unknown",), "bin", 12887),
+        (12892, ("JAVA", "ADF"), "adf", 0),
+        (13087, ("unknown",), "bin", 13087),
+    ]
+else:
+    raise ValueError(f"Invalid configuration: {args.config}")
 
-RANGES = [
-    (0, ("unknown",), None, 0),
-    (12484, ("unknown",), "bin", 12484),
-    (12492, ("JAVA", "JAR"), "jar", 0),
-    (12687, ("unknown",), "bin", 12687),
-    (12692, ("JAVA", "SCP"), "scp", 0),
-    (12887, ("unknown",), "bin", 12887),
-    (12892, ("JAVA", "ADF"), "adf", 0),
-    (13087, ("unknown",), "bin", 13087),
-]
-
-for i in range(0x800, 0x1E000, 8):
+for i in range(LIST_START, LIST_END, 8):
     if (
         data[i : i + 8] != b"\xff\xff\xff\xff\xff\xff\xff\xff"
         and data[i : i + 8] != b"\x00\x00\x00\x00\x00\x00\x00\x00"
@@ -36,12 +55,12 @@ for i in range(0x800, 0x1E000, 8):
             file_st_pv = file_st
             file_data += data[file_st * 0x200 : (file_st + 1) * 0x200]
             file_st = int.from_bytes(
-                data[0x1E40C + file_st * 2 : 0x1E40C + file_st * 2 + 2], "little"
+                data[TABLE + file_st * 2 : TABLE + file_st * 2 + 2], "little"
             )
         file_data = file_data[:file_ln]
         assert file_st_pv == file_ed
         assert len(file_data) == file_ln
-        file_id = (i - 0x800) // 8
+        file_id = (i - LIST_START) // 8
         off = -1
         while RANGES[off][0] > file_id:
             off -= 1
@@ -55,7 +74,10 @@ for i in range(0x800, 0x1E000, 8):
                 ext = "gif"
             elif file_data[:3] == b"FWS" or file_data[:3] == b"CWS":
                 ext = "swf"
-            elif file_data[:12] == b"\x00\x00\x00\x1cftypn5lm":
+            elif file_data[:12] in [
+                b"\x00\x00\x00\x1cftypn5lm",
+                b"\x00\x00\x00\x1cftypn5mv",
+            ]:
                 ext = "3gp"
             elif file_data[:12] == b"\x00\x00\x00\x1cftypmmp4":
                 ext = "mp4"
