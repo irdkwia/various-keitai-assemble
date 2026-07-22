9161cde4603b9309c45793348e725a1e673e7c8d
irdkwia <irdkwia2000@outlook.com>
2026-07-22 16:07:57 +0200

    Add SO505i FTL support

---
+++ assemble_so505i.py
@@ -0,0 +1,91 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="Keitai SO505i Assemble")
+parser.add_argument("input")
+parser.add_argument("output")
+parser.add_argument(
+    "-ob",
+    "--input-oob",
+    default=None,
+    help="If not specified, a file with the same name as the input NAND file and the extension '.oob' in the same folder will be automatically used.",
+)
+parser.add_argument(
+    "-p",
+    "--partition",
+    help="Use partition system.",
+    action=argparse.BooleanOptionalAction,
+)
+
+args = parser.parse_args()
+
+os.makedirs(args.output, exist_ok=True)
+
+MAX_BLOCKS = 1000
+
+out_oob = args.input_oob or os.path.join(
+    os.path.dirname(args.input),
+    f"{os.path.splitext(os.path.basename(args.input))[0]}.oob",
+)
+partition = args.partition
+block = True
+f = None
+while block:
+    try:
+        with open(args.input, "rb") as nand:
+            with open(out_oob, "rb") as oob:
+                data = nand.read(0x200)
+                spare = oob.read(0x10)
+                prev = (0xFFFF, 0xFFFF)
+                d = {}
+                while len(data) > 0:
+                    id_1 = int.from_bytes(spare[6:8], "big")
+                    id_2 = int.from_bytes(spare[11:13], "big")
+                    assign = (
+                        int.from_bytes(spare[2:4], "big")
+                        if partition
+                        else (0x1101 if id_1 != 0xFFFF else 0xFFFF)
+                    )
+                    assert id_1 == id_2, f"Error: %04X, %04X vs %04X mismatch" % (
+                        assign,
+                        id_1,
+                        id_2,
+                    )
+                    t = prev
+                    prev = (id_1, assign)
+                    if prev != (0xFFFF, 0xFFFF):
+                        actual_id = (id_1 & 0xFFF) >> 1
+                        actual_assign = ((assign & 0xFF) >> 1, assign >> 12)
+                        if prev != t:
+                            d[actual_assign[0]] = d.get(actual_assign[0], {})
+                            d[actual_assign[0]][actual_assign[1]] = d[
+                                actual_assign[0]
+                            ].get(actual_assign[1], {})
+                            assert (
+                                actual_id not in d[actual_assign[0]][actual_assign[1]]
+                            ), f"Error: %04X, %04X already exists" % (assign, id_1)
+                            d[actual_assign[0]][actual_assign[1]][actual_id] = (
+                                bytearray(data)
+                            )
+                        else:
+                            d[actual_assign[0]][actual_assign[1]][actual_id] += data
+                    data = nand.read(0x200)
+                    spare = oob.read(0x10)
+        block = False
+    except Exception as e:
+        partition = not partition
+        if f is not None:
+            raise f
+        f = e
+
+for k, v in d.items():
+    with open(os.path.join(args.output, "partition_%02X.bin" % k), "wb") as file:
+        for k2, v2 in sorted(v.items()):
+            ldata = len(v2[min(v2)])
+            m = max(v2) + 1
+            assert m <= MAX_BLOCKS, f"{m} above {MAX_BLOCKS}"
+            for x in range(MAX_BLOCKS):
+                if x in v2:
+                    file.write(v2[x])
+                else:
+                    file.write(bytes(ldata))
